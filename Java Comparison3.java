9e59cd172a9a947a2095d53267e41b1bd339cfdc
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:38:37 +0600

    Create Java Comparison3.java

---
+++ Java Comparison3.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+           int passwordLength=5;
+           System.out.println(passwordLength>=8);
+           System.out.println(passwordLength<8);
+    }
+}
