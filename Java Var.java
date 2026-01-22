5aa60dd41392c922de4b0a47ce14d4b2ade35574
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:39:26 +0600

    Create Java Var.java

---
+++ Java Var.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[]args){
+      var myNum=5;
+      var myDouble=9.98;
+      var myChar='D';
+      var myBoolean=true;
+      var myString="Hello";
+      System.out.println(myNum);
+      System.out.println(myDouble);
+      System.out.println(myChar);
+      System.out.println(myBoolean);
+      System.out.println(myString);
+    }
+}
