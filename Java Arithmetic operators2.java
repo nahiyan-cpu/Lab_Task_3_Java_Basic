bfd6d052cfb07ee1ae0dd6ff2f49c72d0e7c5559
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:22:46 +0600

    Create Java Arithmetic operators2.java

---
+++ Java Arithmetic operators2.java
@@ -0,0 +1,13 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+       int a= 10;
+       int b=3;
+       System.out.println(a/b);
+
+       double c=10.0d;
+       double d=3.0d;
+       System.out.println(c/d);
+    }
+}
