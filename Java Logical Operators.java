cce7c1ea59ace23b5a5d88090b4097c4b1ee13c7
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:42:09 +0600

    Create Java Logical Operators.java

---
+++ Java Logical Operators.java
@@ -0,0 +1,11 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+          boolean isLoggedIn=true;
+          boolean isAdmin=false;
+          System.out.println("Regular user: " + (isLoggedIn && !isAdmin));
+          System.out.println("Has access:: " + (isLoggedIn || isAdmin));
+          System.out.println("Not logged in: " + (!isLoggedIn));
+    }
+}
