4ed14728223bbef13bfee5ec661ffa6ebf07ade7
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:34:08 +0600

    Create Java Assignment Operators Real Life Examples.java

---
+++ Java Assignment Operators Real Life Examples.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+            int savings = 100;
+            savings+=50;
+            System.out.println("Total savings: " + savings);
+    }
+}
