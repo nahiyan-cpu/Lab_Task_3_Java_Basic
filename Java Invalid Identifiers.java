3c9d5f774b42e94217bd9421be339f9662008604
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:00:34 +0600

    Create Java Invalid Identifiers.java

---
+++ Java Invalid Identifiers.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[]args){
+     int 2ndNumber=5;//Cannot start with a digit
+     int my var = 10; //Cannot contain spaces
+     int int = 20;//Cannot use reserved Keywords
+       
+    }
+}
