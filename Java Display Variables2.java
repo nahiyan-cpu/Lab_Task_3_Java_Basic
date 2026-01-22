b2594f50c1f0959b4e51eaf3bed8ed7039be22d9
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 18:49:27 +0600

    Create Java Display Variables2.java

---
+++ Java Display Variables2.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[]args){
+       String firstName = "John";
+       String lastName="Doe";
+       String fullName=firstName + lastName;
+       System.out.println(fullName);
+
+       int x=5;
+       int y=6;
+       System.out.println(x+y);
+       System.out.println("The sum is " + x+y);
+       System.out.println("The sum is " + (x+y));
+    }
+}
