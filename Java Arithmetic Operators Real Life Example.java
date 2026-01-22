2f8d90c08f10a662cd353d0ebe8c8f1084dff99c
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:26:34 +0600

    Create Java Arithmetic Operators Real Life Example.java

---
+++ Java Arithmetic Operators Real Life Example.java
@@ -0,0 +1,13 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+      int peopleInRoom=0;
+      peopleInRoom++;
+      peopleInRoom++;
+      peopleInRoom++;
+      System.out.println(peopleInRoom);
+      peopleInRoom--;
+      System.out.println(peopleInRoom);
+    }
+}
