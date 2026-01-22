e54b8dc6d11a295f84bf9b7f7bc51642e6e4c5e5
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:32:25 +0600

    Create Java Assignment operators2.java

---
+++ Java Assignment operators2.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+            int x = 10;
+            x+=5;
+            System.out.println(x);
+    }
+}
