074f8771a3cbef57b228d288671855771c160ce8
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 18:58:30 +0600

    Create Java Identifiers.java

---
+++ Java Identifiers.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args){
+     //Good
+     int minutesPerHour=60;
+
+     //OK,but not so easy to understand what m actually is
+     int m=60;
+       
+    }
+}
