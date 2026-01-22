01dd4b48ff7f55d8e6aa040856f689cf0468cb4f
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:35:26 +0600

    Create Java Data Types Example.java

---
+++ Java Data Types Example.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[]args){
+       int items=50;
+       float costPerItem=9.99f;
+       float totalCost=items*costPerItem;
+       char currency='$';
+
+       System.out.println("Number of items: " + items);
+       System.out.println("cost per item: " + costPerItem + currency);
+       System.out.println("Total cost = " + totalCost + currency);
+    }
+}
