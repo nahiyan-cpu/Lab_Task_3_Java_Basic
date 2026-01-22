eb4fde33b021eeebb4ae603b76b79842b15f3497
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:03:52 +0600

    Create Java Constants.java

---
+++ Java Constants.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[]args){
+        //You should declare variables as final when their values should never change.
+        final int MINUTES_PER_HOUR=60;
+        final int BIRTHYEAR=1980;
+ }
+}
