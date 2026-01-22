d2931f3cc14bbe9e74f58b038ad5f8f427f7e742
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:30:09 +0600

    Create Java Characters.java

---
+++ Java Characters.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[]args){
+        char myGrade='B';
+        System.out.println(myGrade);
+        char myVar1=65,myVar2=66,myVar3=67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+    
+    }
+}
