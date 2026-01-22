5fd9dfbe55cce5e7d03e04af2ea38958178aa6a9
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 18:31:03 +0600

    Create Java Variables.java

---
+++ Java Variables.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        String name = "John";
+        int myNum=15;
+        
+        System.out.println(myNum);
+        System.out.println(name);
+    }
+}
