66721624a83f588546b9e139d440c316d3431ce1
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:17:29 +0600

    Create Java Data Types.java

---
+++ Java Data Types.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+       int myNum=5;//myNum is an int
+       //myNum="Hello";//Error:cannot assign a string to an int
+
+       String myText="Hi";//myText is a string
+       //myText=123;   //Error:cannot assign a number to a string
+ }
+}
