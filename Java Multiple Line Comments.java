7e43be065d84968ae74b13415cc815b38f046e38
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 18:22:13 +0600

    Create Java Multiple Line Comments.java

---
+++ Java Multiple Line Comments.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[]args){
+        /*
+        1
+        2
+        3 */
+       System.out.println("Hello World!");
+       
+      
+    }
+}
