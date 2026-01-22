f6c3ae9eba4a533d24d34e9a959ecff5d2bb66dc
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:35:56 +0600

    Create Java Comparison.java

---
+++ Java Comparison.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main{
+    public static void main(String[]args){
+            int x=5;
+            int y= 3;
+            System.out.println(x > y);
+    }
+}
