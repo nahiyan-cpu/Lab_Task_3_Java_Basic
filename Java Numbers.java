afab04b540ad708fe4aef2b537df1f4da5f375ea
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:23:27 +0600

    Create Java Numbers.java

---
+++ Java Numbers.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[]args){
+        byte myNum1=100;
+        System.out.println(myNum1);
+        short myNum2 = 5000;
+        System.out.println(myNum2);
+        int myNum3 = 100000;
+        System.out.println(myNum3);
+
+        long myNum4=15000000000L;
+        System.out.println(myNum4);
+
+        float myNum5=5.75f;
+        System.out.println(myNum5);
+
+        double myNum6=19.99d;
+        System.out.println(myNum6);
+
+
+
+    }
+}
