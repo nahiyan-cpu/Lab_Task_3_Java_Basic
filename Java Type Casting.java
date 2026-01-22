eafd1546b06e84cf98052e7ee01ef84ef7b375a1
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 20:02:09 +0600

    Create Java Type Casting.java

---
+++ Java Type Casting.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args){
+     int maxScore=500;
+     int userScore=423;
+
+     double percentage=(double)userScore / maxScore * 100.0d;
+
+     System.out.println("User's percentage is " + percentage);
+    }
+}
