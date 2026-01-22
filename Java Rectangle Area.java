b60050e29bc3268b6a054d9334313bd17757d3f0
Syed Nahiyan Alam <nahiyanctg07@gmail.com>
2026-01-22 19:13:41 +0600

    Create Java Rectangle Area.java

---
+++ Java Rectangle Area.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[]args){
+       //Create Integer variables
+       int length = 4;
+       int width = 6;
+       int area;
+
+       //Calculate the area of rectangle
+       area=length*width;
+       //Print variables
+       System.out.println("Length is: " + length);
+       System.out.println("Width is: " + width);
+       System.out.println("Area of the rectangle is: " + area);
+ }
+}
